fa814a3c32f54dd79501158edd7510ec0d2d41ec
JaberHPranto <jhpranto53@gmail.com>
2021-10-14 11:31:02 +0600

    state

--- src/pages/hello-state/state-provider.ts
+++ src/pages/hello-state/state-provider.ts
@@ -1,14 +1,82 @@
 export function getState(message: string) {
   switch (message) {
     case "enter":
-      return "open";
+      return;
 
     case "payOk":
-      return "open";
+      return;
 
     case "payFailed":
-      return "closed";
+      return;
     default:
-      return "processing";
+      return;
+  }
+}
+
+// State Pattern
+interface GateState {
+  enter(): string;
+  payOk(): string;
+  payFailed(): string;
+}
+
+// Context
+class Gate {
+  state: GateState;
+  constructor(state: GateState) {
+    this.state = state;
+  }
+
+  changeState(state: GateState) {
+    this.state = state;
+  }
+
+  enter(): void {
+    this.state.enter();
+  }
+
+  payOk(): void {
+    this.state.payOk();
+  }
+  payFailed(): void {
+    this.state.payFailed();
+  }
+}
+
+// Concrete class
+class OpenGate implements GateState {
+  gate: Gate;
+  constructor(gate: Gate) {
+    this.gate = gate;
+  }
+  enter(): string {
+    this.gate.changeState(new CloseGate(this.gate));
+    return "close";
+  }
+  payOk(): string {
+    this.gate.changeState(new OpenGate(this.gate));
+    return "open";
+  }
+  payFailed(): string {
+    this.gate.changeState(new OpenGate(this.gate));
+    return "open";
+  }
+}
+class CloseGate implements GateState {
+  gate: Gate;
+  constructor(gate: Gate) {
+    this.gate = gate;
+  }
+  enter(): string {
+    this.gate.changeState(new CloseGate(this.gate));
+    return "close";
+  }
+  payOk(): string {
+    this.gate.changeState(new OpenGate(this.gate));
+    return "open";
+  }
+  payFailed(): string {
+    this.gate.changeState(new CloseGate(this.gate));
+    return "open";
   }
 }
